1f392fedcb9fcb8bd35b36f8fde704d422123f01
Aluno <Aluno@DESKTOP-DMGHJ3C>
2024-04-27 12:12:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numerosPares = 0;
+double numerosImpares = 1;
+
+Console.WriteLine("Números pares:");
+
+while (numerosPares < 100)
+{
+    numerosPares += 2;
+    Console.Write(numerosPares + " - ");
+}
+
+Console.WriteLine("Números ímpares:");
+
+while (numerosImpares < 98)
+{
+    numerosImpares += 2 ;
+    Console.Write(numerosImpares + " - ");
+}
